3bc1123d26bf9f07d2495f21d5cab3bbebe9e5af
Maglione Riccardo <riccardo99maglione@gmail.com>
2021-11-26 12:30:58 +0100

    Added script RebelRobotCharacter

---
+++ Source/RebelRobot/Public/RebelRobotCharacter.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "RebelRobotCharacter.generated.h"
+
+UCLASS()
+class REBELROBOT_API ARebelRobotCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ARebelRobotCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
