4422834020fa0426c83fbeb17c479b96aad75a55
Maglione Riccardo <riccardo99maglione@gmail.com>
2022-05-07 15:06:42 +0200

    Converted variables and OnUnhovered of radial in C++

--- Source/RebelRobot/Private/RR_UWidget_Menu_Radial.cpp
+++ Source/RebelRobot/Private/RR_UWidget_Menu_Radial.cpp
@@ -3,9 +3,101 @@
 
 #include "RR_UWidget_Menu_Radial.h"
 #include "..\Public\RR_UWidget_Menu_Radial.h"
+#include "Components/Button.h"
+#include "Components/Image.h"
 
 bool URR_UWidget_Menu_Radial::Initialize()
 {
     Super::Initialize();
+
+    Button_Dash->OnUnhovered.AddDynamic(this, &URR_UWidget_Menu_Radial::Radial_DashUnhovered);
+    Button_Shield->OnUnhovered.AddDynamic(this, &URR_UWidget_Menu_Radial::Radial_ShieldUnhovered);
+    Button_BlackHole->OnUnhovered.AddDynamic(this, &URR_UWidget_Menu_Radial::Radial_BlackHoleUnhovered);
+    Button_ShockWave->OnUnhovered.AddDynamic(this, &URR_UWidget_Menu_Radial::Radial_ShockWaveUnhovered);
+
     return true;
 }
+
+void URR_UWidget_Menu_Radial::Radial_HoverDelegate()
+{
+    //SetBrush
+    //AllbAbilitySelected1 = false
+    //ThisbAbilitySelected = true
+    //Get player character and cast to v4
+    //Get Ability component
+    //Get ACCooldown
+    //Append String and convert to text
+    //Set text <- TextBlockAbility
+}
+
+#pragma region
+void URR_UWidget_Menu_Radial::Radial_DashHover()
+{
+    Image_Dash->SetBrush(InBrush_Dash_Selected);
+    bHover_BlackHole_Selected = false;
+    bHover_Shield_Selected = false;
+    bHover_ShockWave_Selected = false;
+    bHover_Dash_Selected = true;
+}
+
+void URR_UWidget_Menu_Radial::Radial_ShieldHover()
+{
+    Image_Shield->SetBrush(InBrush_Shield_Selected);
+    bHover_Dash_Selected = false;
+    bHover_BlackHole_Selected = false;
+    bHover_ShockWave_Selected = false;
+    bHover_Shield_Selected = true;
+}
+
+void URR_UWidget_Menu_Radial::Radial_BlakcHoleHover()
+{
+    Image_BlackHole->SetBrush(InBrush_BlackHole_Selected);
+    bHover_Dash_Selected = false;
+    bHover_Shield_Selected = false;
+    bHover_ShockWave_Selected = false;
+    bHover_BlackHole_Selected = true;
+}
+
+void URR_UWidget_Menu_Radial::Radial_ShockWaveHover()
+{
+    Image_ShockWave->SetBrush(InBrush_ShockWave_Selected);
+    bHover_Dash_Selected = false;
+    bHover_Shield_Selected = false;
+    bHover_BlackHole_Selected = false;
+    bHover_ShockWave_Selected = true;
+}
+#pragma endregion Radial Hover Delegate
+
+#pragma region
+void URR_UWidget_Menu_Radial::Radial_DashUnhovered()
+{
+    if (!bDash_Selected) {
+        Image_Dash->UImage::SetBrush(InBrush_NotSelected);
+        bHover_Dash_Selected = false;
+    }
+}
+
+void URR_UWidget_Menu_Radial::Radial_ShieldUnhovered()
+{
+    if (!bShield_Selected) {
+        Image_Shield->UImage::SetBrush(InBrush_NotSelected);
+        bHover_Shield_Selected = false;
+    }
+}
+
+void URR_UWidget_Menu_Radial::Radial_BlackHoleUnhovered()
+{
+    if (!bBlackHole_Selected) {
+        Image_BlackHole->UImage::SetBrush(InBrush_NotSelected);
+        bHover_BlackHole_Selected = false;
+    }
+}
+
+void URR_UWidget_Menu_Radial::Radial_ShockWaveUnhovered()
+{
+    if (!bShockWave_Selected) {
+        Image_ShockWave->UImage::SetBrush(InBrush_NotSelected);
+        bHover_ShockWave_Selected = false;
+    }
+}
+#pragma endregion Radial Unhovered Delegate
--- Source/RebelRobot/Public/RR_UWidget_Menu_Radial.h
+++ Source/RebelRobot/Public/RR_UWidget_Menu_Radial.h
@@ -15,4 +15,84 @@ class REBELROBOT_API URR_UWidget_Menu_Radial : public UUserWidget
 	GENERATED_BODY()
 	
 	virtual bool Initialize();
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "RR | Radial")
+		bool bDash_Selected;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "RR | Radial")
+		bool bShield_Selected;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "RR | Radial")
+		bool bShockWave_Selected;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "RR | Radial")
+		bool bBlackHole_Selected;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "RR | Radial")
+		bool bHover_Dash_Selected;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "RR | Radial")
+		bool bHover_Shield_Selected;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "RR | Radial")
+		bool bHover_ShockWave_Selected;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "RR | Radial")
+		bool bHover_BlackHole_Selected;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "RR | Radial")
+		FSlateBrush InBrush_Dash_Selected;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "RR | Radial")
+		FSlateBrush InBrush_Shield_Selected;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "RR | Radial")
+		FSlateBrush InBrush_BlackHole_Selected;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "RR | Radial")
+		FSlateBrush InBrush_ShockWave_Selected;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "RR | Radial")
+		FSlateBrush InBrush_NotSelected;
+
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
+		class UButton* Button_Dash;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
+		class UButton* Button_Shield;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
+		class UButton* Button_BlackHole;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
+		class UButton* Button_ShockWave;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
+		class UImage* Image_Dash;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
+		class UImage* Image_Shield;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
+		class UImage* Image_BlackHole;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
+		class UImage* Image_ShockWave;
+
+
+
+
+	UFUNCTION(BlueprintCallable, Category = "RR | Radial")
+		void Radial_HoverDelegate();
+
+
+
+	UFUNCTION(BlueprintCallable, Category = "RR | Radial")
+		void Radial_DashHover();
+	UFUNCTION(BlueprintCallable, Category = "RR | Radial")
+		void Radial_ShieldHover();
+	UFUNCTION(BlueprintCallable, Category = "RR | Radial")
+		void Radial_BlakcHoleHover();
+	UFUNCTION(BlueprintCallable, Category = "RR | Radial")
+		void Radial_ShockWaveHover();
+
+	UFUNCTION(BlueprintCallable, Category = "RR | Radial")
+		void Radial_DashUnhovered();
+	UFUNCTION(BlueprintCallable, Category = "RR | Radial")
+		void Radial_ShieldUnhovered();
+	UFUNCTION(BlueprintCallable, Category = "RR | Radial")
+		void Radial_BlackHoleUnhovered();
+	UFUNCTION(BlueprintCallable, Category = "RR | Radial")
+		void Radial_ShockWaveUnhovered();
+
 };
